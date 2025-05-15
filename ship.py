93f0f3de3d2cee0403c39d6c40271a3e27532e14
Pushpendra <pushpendrauikey963@gmail.com>
2025-05-15 18:27:36 +0530

    Creating the ship class

---
+++ ship.py
@@ -0,0 +1,22 @@
+import pygame
+""" Pygame treats each element of the game as a surface. It is easy to work with their 
+    coordinates then
+    """
+class Ship:
+    """ A class to manage the ship. """
+    
+    def __init__(self, ai_game):
+        """ Initialize the ship and set its initial position. """
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect() # get_rect() returns a rect object representing the screen.
+
+        # load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp') #returns the surface representing the image
+        self.rect = self.image.get_rect()
+
+        # start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """ Draw the ship at its current location. """
+        self.screen.blit(self.image, self.rect)
