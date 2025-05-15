077b7ee6f7471ad3cb91d15dddd7b4cec86fab2b
Pushpendra <pushpendrauikey963@gmail.com>
2025-05-15 18:57:10 +0530

    Refactored the code checking events at the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,15 +25,19 @@ class AlienInvasion:
         """ Start the main loop for the game. """
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
             
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color) # since every new frame overwrites the old one.
             self.ship.blitme()  # Draw the ship in the current surface.
             # Make the most recently drawn screen visible (Doesn't remember previous frames).
             pygame.display.flip()
+    
+    def _check_events(self):
+        """ Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
             
 if __name__ == '__main__':
     # Make a game instance, and run the game.
